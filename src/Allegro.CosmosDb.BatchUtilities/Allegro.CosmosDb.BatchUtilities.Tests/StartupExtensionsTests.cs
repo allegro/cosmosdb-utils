c84183d3924891359c189f67ce65382861711076
Jakub Dropia <jakub.dropia@allegro.com>
2023-09-06 16:20:29 +0200

    feat: small refactor of unit tests

--- src/Allegro.CosmosDb.BatchUtilities/Allegro.CosmosDb.BatchUtilities.Tests/StartupExtensionsTests.cs
+++ src/Allegro.CosmosDb.BatchUtilities/Allegro.CosmosDb.BatchUtilities.Tests/StartupExtensionsTests.cs
@@ -10,21 +10,30 @@ using Xunit;
 
 namespace Allegro.CosmosDb.Tests.Unit;
 
+//Connection strings in these tests are fake. They do not point to existing resources, and they do not include real key
 public class StartupExtensionsTests
 {
     private const string ClientNameA = "ClientNameA";
     private const string ClientNameB = "ClinetNameB";
 
-    //Connection strings in these tests are fake. They do not point to existing resources, as they do not include real key
-    
-    [Fact]
-    public void WhenAddingTwoDifferentClients_ShouldRegisterProperCountOfServicesWithoutDuplications()
+    private const string FakeClientConnectionStringA =
+        "AccountEndpoint=https://clienta.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699A==;";
+    private const string FakeClientConnectionStringB =
+        "AccountEndpoint=https://clientb.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699B==;";
+    private static IServiceCollection Init()
     {
         var services = new ServiceCollection();
         services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
         services.AddSingleton<ILogger<BatchRequestHandler>>(NullLogger<BatchRequestHandler>.Instance);
-        services.AddCosmosBatchClient(sp => new CosmosClientBuilder("AccountEndpoint=https://clienta.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699A==;"), ClientNameA);
-        services.AddCosmosBatchClient(sp => new CosmosClientBuilder("AccountEndpoint=https://clientb.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699B==;"), ClientNameB);
+        return services;
+    }
+
+    [Fact]
+    public void WhenAddingTwoDifferentClients_ShouldRegisterProperCountOfServicesWithoutDuplications()
+    {
+        var services = Init();
+        services.AddCosmosBatchClient(sp => new CosmosClientBuilder(FakeClientConnectionStringA), ClientNameA);
+        services.AddCosmosBatchClient(sp => new CosmosClientBuilder(FakeClientConnectionStringB), ClientNameB);
 
         var serviceProvider = services.BuildServiceProvider();
 
@@ -41,12 +50,10 @@ public class StartupExtensionsTests
     [Fact]
     public void WhenAddingTwoSameClients_ShouldThrowException()
     {
-        var services = new ServiceCollection();
-        services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
-        services.AddSingleton<ILogger<BatchRequestHandler>>(NullLogger<BatchRequestHandler>.Instance);
-        services.AddCosmosBatchClient(sp => new CosmosClientBuilder("AccountEndpoint=https://clienta.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699A==;"), ClientNameA);
+        var services = Init();
+        services.AddCosmosBatchClient(sp => new CosmosClientBuilder(FakeClientConnectionStringA), ClientNameA);
 
-        var act = () => services.AddCosmosBatchClient(sp => new CosmosClientBuilder("AccountEndpoint=https://clienta.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699A==;"), ClientNameA);
+        var act = () => services.AddCosmosBatchClient(sp => new CosmosClientBuilder(FakeClientConnectionStringA), ClientNameA);
 
         act.Should()
             .ThrowExactly<InvalidOperationException>("Cosmos batch client with name = 'cosmosclienta' is already registered!");
@@ -55,11 +62,9 @@ public class StartupExtensionsTests
     [Fact]
     public void WhenAddingClients_ShouldBeAbleToGetThemFromProviders()
     {
-        var services = new ServiceCollection();
-        services.AddSingleton<ILoggerFactory, NullLoggerFactory>();
-        services.AddSingleton<ILogger<BatchRequestHandler>>(NullLogger<BatchRequestHandler>.Instance);
-        services.AddCosmosBatchClient(sp => new CosmosClientBuilder("AccountEndpoint=https://clienta.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699A==;"), ClientNameA);
-        services.AddCosmosBatchClient(sp => new CosmosClientBuilder("AccountEndpoint=https://clientb.documents.azure.com:443/;AccountKey=craKHH9vLiQtacNuQ97cZyqchs1NBL3XHw7RkkNDsyca8ZAmc7MPP4xnBO9zRBdNNeFvK4cC3EbZVrzeDf699B==;"), ClientNameB);
+        var services = Init();
+        services.AddCosmosBatchClient(sp => new CosmosClientBuilder(FakeClientConnectionStringA), ClientNameA);
+        services.AddCosmosBatchClient(sp => new CosmosClientBuilder(FakeClientConnectionStringB), ClientNameB);
 
         var serviceProvider = services.BuildServiceProvider();
 
@@ -71,5 +76,7 @@ public class StartupExtensionsTests
         var factoryB = factoryProvider.GetFactory(ClientNameB);
         clientA.Should().NotBe(clientB);
         factoryA.Should().NotBe(factoryB);
+        clientA.Endpoint.Should().Be("https://clienta.documents.azure.com/");
+        clientB.Endpoint.Should().Be("https://clientb.documents.azure.com/");
     }
 }
